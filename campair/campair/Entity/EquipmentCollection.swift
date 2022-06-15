80672029f0fcb9705fcb61dfa57bd17cfd1e90ab
이가은 <82457928+rriver2@users.noreply.github.com>
2022-06-15 23:42:45 +0900

    [Feature] Create an equipmentGroups entity and JSON data

---
+++ campair/campair/Entity/EquipmentCollection.swift
@@ -0,0 +1,12 @@
+//
+//  EquipmentCollection.swift
+//  campair
+//
+//  Created by 이가은 on 2022/06/15.
+//
+
+import Foundation
+
+struct EquipmentCollection: Codable {
+    let equipmentGroups: [EquipmentGroup]
+}
---
+++ campair/campair/Entity/EquipmentGroup.swift
@@ -0,0 +1,13 @@
+//
+//  EquipmentGroup.swift
+//  campair
+//
+//  Created by 이가은 on 2022/06/15.
+//
+
+import Foundation
+
+struct EquipmentGroup: Codable {
+    let name: String
+    let equipments: [Equipment]
+}
