19d6a95a080f64e9b07152e3b33618bf89c080c5
Lee Myeonghwan <leemyeonghwan@Gobans-MacBook.local>
2022-06-18 16:42:01 +0900

    [Feature] #90 Make tabbarUI

--- campair/campair/Application/CampairApp.swift
+++ campair/campair/Application/CampairApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct CampairApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            TabbarView()
         }
     }
 }
---
+++ campair/campair/View/TabbarView.swift
@@ -0,0 +1,33 @@
+//
+//  tabbarView.swift
+//  campair
+//
+//  Created by Lee Myeonghwan on 2022/06/18.
+//
+
+import SwiftUI
+
+struct TabbarView: View {
+    let editorIcon = UIImage(named: "EditorTabbarImage")
+    let dictionaryIcon = UIImage(named: "DictionaryTabbarImage")
+    var body: some View {
+        TabView {
+            EditorMainView()
+                .tabItem {
+                    Image(uiImage: editorIcon!)
+                    Text("에디터")
+                }
+            DictionaryMainView()
+                .tabItem {
+                    Image(uiImage: dictionaryIcon!)
+                    Text("장비사전")
+                }
+        }
+    }
+}
+
+struct TabbarView_Previews: PreviewProvider {
+    static var previews: some View {
+        TabbarView()
+    }
+}
