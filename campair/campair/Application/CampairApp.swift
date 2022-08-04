a99a11d892d0c7306cbc4b2f8e711b2228c27a38
rriver2 <rriver2@naver.com>
2022-08-04 16:17:49 +0900

    [feat] add splash image

--- campair/campair/Application/CampairApp.swift
+++ campair/campair/Application/CampairApp.swift
@@ -7,8 +7,16 @@
 
 import SwiftUI
 
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil) -> Bool {
+        Thread.sleep(forTimeInterval: 1)
+        return true
+    }
+}
+
 @main
 struct CampairApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
     var body: some Scene {
         WindowGroup {
             TabbarView()
